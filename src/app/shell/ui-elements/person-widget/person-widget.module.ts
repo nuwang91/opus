8bea968bab58b56d073cf4fb42ad06c470e0b801
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2018-08-11 16:21:14 +0530

    Person Widget & Shell

---
+++ src/app/shell/ui-elements/person-widget/person-widget.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonWidgetComponent } from './person-widget.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [PersonWidgetComponent],
+  declarations: [PersonWidgetComponent]
+})
+export class PersonWidgetModule { }
