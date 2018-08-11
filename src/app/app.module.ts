e762a62cf684aefe89a4d55559fae9c6a2d5cd0b
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2018-08-11 12:17:38 +0530

    Update path of login properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginModule } from './login/login/login.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
