78f95df0f4d910e71710cf0669aa52fe5ee98bc8
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2018-08-17 23:49:05 +0530

    Changed package json

--- src/app/shell/pages/attendance/attendance.module.ts
+++ src/app/shell/pages/attendance/attendance.module.ts
@@ -4,16 +4,13 @@ import { AttendanceComponent } from "./attendance.component";
 import { ContainerModule } from "../../container/container.module";
 import { SelectorModule } from "../../ui-elements/selector/selector.module";
 import { MatButtonModule } from "@angular/material/button";
-import { DlDateTimePickerDateModule } from "angular-bootstrap-datetimepicker";
-
 
 @NgModule({
   imports: [
     CommonModule,
     ContainerModule,
     SelectorModule,
-    MatButtonModule,
-    DlDateTimePickerDateModule
+    MatButtonModule
   ],
   exports: [AttendanceComponent],
   declarations: [AttendanceComponent]
