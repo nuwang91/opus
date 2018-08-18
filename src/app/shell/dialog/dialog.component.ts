23a1266c146d7b6a47ffadb6f2e7952bca12975c
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2018-08-18 19:57:36 +0530

    Dialog fields added

--- src/app/shell/dialog/dialog.component.ts
+++ src/app/shell/dialog/dialog.component.ts
@@ -12,6 +12,25 @@ export class DialogComponent implements OnInit {
 
   constructor() { }
 
+  public projects: Array<{ id: number, name: string }> = [
+    { id: 1, name: "Dialog Mega Run" },
+    { id: 2, name: "Kaunas" },
+    { id: 3, name: "Pavilnys" },
+    { id: 4, name: "Pabradė" },
+    { id: 5, name: "Klaipėda" }
+  ];
+
+  public users: Array<{ id: number, name: string }> = [
+    { id: 1, name: "Nuwan" },
+    { id: 2, name: "Chamira" },
+    { id: 3, name: "Indika" },
+    { id: 4, name: "Lochana" },
+    { id: 5, name: "Prabath" }
+  ];
+
+  public selectedProject: string;
+  public selectedUser: string;
+
   public ngOnInit(): void {
   }
 
