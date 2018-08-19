54e0f53d9490e11a27b23d074d5019e5f2250dd0
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2018-08-19 13:37:09 +0530

    Half of dashboard

--- src/app/shell/ui-elements/person-widget/person-widget.component.ts
+++ src/app/shell/ui-elements/person-widget/person-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "app-person-widget",
@@ -7,21 +7,39 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PersonWidgetComponent implements OnInit {
 
+  @Input()
+  public listMode: boolean = false;
+
+  @Input()
   public person: PersonInterface = {
+    id: "testID",
     name: "Bill Gates",
     usertype: "Admin",
     picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ4dm9USa9L-OYLcjgvTjZ5TnfxLRSs1stSugJ2YkRlM48xmg6kw"
   };
+
+  // public person: PersonInterface = {
+  //   id: "testID",
+  //   name: "Bill Gates",
+  //   usertype: "Admin",
+  //   picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ4dm9USa9L-OYLcjgvTjZ5TnfxLRSs1stSugJ2YkRlM48xmg6kw"
+  // };
 
   constructor() { }
 
   public ngOnInit(): void {
   }
 
+  // public getType(): string {
+  //   return this.listMode ? this.person.designation : this.person.type
+  // }
+
 }
 
 export interface PersonInterface {
+  id: string;
   name: string;
   usertype: string;
+  designation?: string;
   picture?: string;
 }
