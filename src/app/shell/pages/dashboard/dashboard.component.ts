b4a73b419f71e9625f9347d7b082463adf385a16
Nuwan Gunaratne <nuwan.gunaratne@ifsworld.com>
2018-08-19 23:15:34 +0530

    Dashboard scrolling

--- src/app/shell/pages/dashboard/dashboard.component.ts
+++ src/app/shell/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit , ChangeDetectorRef } from "@angular/core";
+import { Component, OnInit , ChangeDetectorRef, ViewChild, ElementRef, AfterViewInit, OnDestroy } from "@angular/core";
 import { faFilter, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { EMPLOYEES } from "../../../../mock-data/mock-employees";
 import { PersonInterface } from "../../ui-elements/person-widget/person-widget.component";
 import { CardInterface, CardComponent } from "../../ui-elements/card/card.component";
+import { Observable, fromEvent as observableFrom, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.scss"]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
 
   public filter: any = faFilter;
   public plusCircle: any = faPlusCircle;
@@ -17,11 +20,36 @@ export class DashboardComponent implements OnInit {
   private personId: number | undefined = undefined;
   private dialogVisible: boolean = false;
 
-  // private showAddIcon: boolean = false;
+  private ngVerticalListPeople$: Observable<Event>;
+  private ngVerticalListEvents$: Observable<Event>;
+  private destroy$: Subject<void> = new Subject();
+
+  private scrollingPeopleList: boolean = false;
+  private scrollingEventsList: boolean = false;
+
+  @ViewChild("ngVerticalListPeople")
+  public ngVerticalListPeople: ElementRef;
+
+  @ViewChild("ngVerticalListEvents")
+  public ngVerticalListEvents: ElementRef;
+
 
   constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
   public ngOnInit(): void {
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+
+  public ngAfterViewInit(): void {
+    if (this.ngVerticalListPeople.nativeElement && this.ngVerticalListEvents.nativeElement) {
+      this.ngVerticalListPeople$ = observableFrom(this.ngVerticalListPeople.nativeElement, "scroll");
+      this.ngVerticalListEvents$ = observableFrom(this.ngVerticalListEvents.nativeElement, "scroll");
+      this.subscribeToVerticalListPeopleScroll();
+      this.subscribeToVerticalListEventsScroll();
+    }
   }
 
   public get employees(): PersonInterface[] {
@@ -73,5 +101,29 @@ export class DashboardComponent implements OnInit {
     if (close) {
       this.addCard();
     }
+  }
+
+  private subscribeToVerticalListPeopleScroll(): void {
+    this.ngVerticalListPeople$.pipe(takeUntil(this.destroy$)).subscribe((event: Event) => {
+      if (!this.scrollingEventsList) {
+        this.scrollingPeopleList = true;
+        this.ngVerticalListEvents.nativeElement.scrollTop = event.srcElement.scrollTop;
+        this.changeDetectorRef.markForCheck();
+      } else {
+        this.scrollingEventsList = false;
+      }
+    });
+  }
+
+  private subscribeToVerticalListEventsScroll(): void {
+    this.ngVerticalListEvents$.pipe(takeUntil(this.destroy$)).subscribe((event: Event) => {
+      if (!this.scrollingPeopleList) {
+        this.scrollingEventsList = true;
+        this.ngVerticalListPeople.nativeElement.scrollTop = event.srcElement.scrollTop;
+        this.changeDetectorRef.markForCheck();
+      } else {
+        this.scrollingPeopleList = false;
+      }
+    });
   }
 }
